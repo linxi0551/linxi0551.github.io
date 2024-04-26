0523e553771622aed202f318d9f47edebb018018
linxi0551 <376575942@qq.com>
2024-04-26 16:33:36 +0800

    测试

--- deploy.sh
+++ deploy.sh
@@ -4,21 +4,20 @@
 set -e
 
 
-push_addr=git@https://github.com/linxi0551/blogs.git # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
-commit_info=`git describe --all --always --long`
-dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=main # 推送的分支
-
 # 生成静态文件
-npm run build
+npm run build:win
 
 # 进入生成的文件夹
-cd $dist_path
+cd docs/.vuepress/dist
 
+# 创建git的本地仓库，提交修改
 git init
 git add -A
-git commit -m "deploy, $commit_info"
-git push -f $push_addr HEAD:$push_branch
+git commit -m 'deploy'
+
+# 覆盖式地将本地仓库发布至github，因为发布不需要保留历史记录
+# 格式为：git push -f git@github.com:'用户名'/'仓库名'.git master
+git push -f git@github.com:linxi0551/blogs.github.io.git master
 
 cd -
-rm -rf $dist_path
+
